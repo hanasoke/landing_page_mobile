ed07b166a883a6bc79ae22b76dee4d78605addca
hanasbp <hanasoke@gmail.com>
2026-06-05 17:31:23 +0700

    updated mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -69,54 +69,78 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            // SEARCH BAR
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari kos (lokasi / nama)',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            // FILTER BUTTON
+            Row(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.filter_list),
+                  label: Text('Filter'),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 16),
+
+            // LIST KOS
+            Expanded(
+              child: ListView(
+                children: [
+                  kosCard(
+                    nama: 'Kamar Konohagakure',
+                    lokasi: 'Dekat Kampus',
+                    harga: 'Rp 800.000 / bulan',
+                  ),
+                  kosCard(
+                    nama: 'Kamar Iwagakure ',
+                    lokasi: 'Dekat jalan sirojul munir',
+                    harga: 'Rp 1.200.000 / bulan',
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget kosCard({
+  required String nama,
+  required String lokasi,
+  required String harga,
+}) {
+  return Card(
+    margin: const EdgeInsets.only(bottom: 12),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    child: ListTile(
+      leading: Icon(Icons.home, size: 40),
+      title: Text(nama, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(lokasi),
+      trailing: Text(harga, style: TextStyle(color: Colors.green)),
+    ),
+  );
 }
